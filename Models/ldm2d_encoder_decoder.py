e5085f35f559d02f2fb93b7d899e4a4fd51c3746
Balázs Tibor Morvay <balazsmorvay@yahoo.com>
2024-02-26 15:40:40 +0100

    Small changes, group norm groups increased to 32

--- Models/ldm2d_encoder_decoder.py
+++ Models/ldm2d_encoder_decoder.py
@@ -64,7 +64,7 @@ def nonlinearity(x):
     return x*torch.sigmoid(x)
 
 
-def Normalize(in_channels, num_groups=8):
+def Normalize(in_channels, num_groups=32):
     return torch.nn.GroupNorm(num_groups=num_groups, num_channels=in_channels, eps=1e-6, affine=True)
 
 
